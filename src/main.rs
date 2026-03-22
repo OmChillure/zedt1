2cab1d77f2de89e71aacec320a71306b71002b05
omchillure <omchillure4@gmail.com>
2026-03-22 11:56:19 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ fn greet(name: &str) -> String {
 fn main() {
     let msg = greet("world");
     let msg2 = greet("world");
-    let msg3 = greet("world");
     println!("{}", msg);
     println!("{}", msg2);
-    println!("{}", msg3);
 }
